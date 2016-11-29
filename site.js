bee510a7b51edf92a605eb9fd9e7405994d31c68
fwahid <fwahid@hawk.iit.edu>
2016-11-28 19:25:28 -0600

    fixed sapcing

--- site.js
+++ site.js
@@ -1,14 +1,33 @@
-$.noConflict();
-(function($) {
-  $(document).ready(function() {
-    $('#uc-form').on('submit', function(e) {
-      $('#loading').empty();
-      $('#loading').append('Loading...');
+/**
+ * On DOMReady initialize page functionality
+ */
+$(document).ready(function(){
 
-var myIndex = 0;
-carousel();
+    // Test we load this file
+    alert("READY!");
 
-$(function carousel() {
+    //Add functionality into the menu buttons
+    prepareMenu();
+
+function prepareMenu()
+{
+    $("#menu li").click(
+        function () {
+            $("#menu li").each(
+                function(){
+                    $(this).removeClass("active");
+                }
+            );
+            $(this).addClass("active");
+            HideFiles($(this).children().html());
+        return false;
+    });
+    //Select the first as default
+    $("#menu li:first").click();
+
+function carousel() {
+  var myIndex = 0;
+  carousel();
     var i;
     var x = document.getElementsByClassName("slides");
     for (i = 0; i < x.length; i++) {
@@ -18,8 +37,18 @@ $(function carousel() {
     if (myIndex > x.length) {myIndex = 1}    
     x[myIndex-1].style.display = "block";  
     setTimeout(carousel, 2000); // Change image every 2 seconds
-})(jQuery);
+}
 
+function myMap() {
+  var mapCanvas = document.getElementById("map");
+  var mapOptions = {
+    center: new google.maps.LatLng(51.5, -0.2),
+    zoom: 5
+  }
+  var map = new google.maps.Map(mapCanvas, mapOptions);
+});
+};
+};
 
 
 
